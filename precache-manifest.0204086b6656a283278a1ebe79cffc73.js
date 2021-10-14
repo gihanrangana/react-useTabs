bee8288b9b4a0fc021d036b3178d4f28e57d5240
Gihan Rangana <itsmerangana@gmail.com>
2021-10-14 23:43:05 +0530

    Updates

---
+++ precache-manifest.0204086b6656a283278a1ebe79cffc73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb2888f84084b052f0a8f1b56941062",
+    "url": "/react-useTabs/index.html"
+  },
+  {
+    "revision": "ecec7fc8af9194f0a0f2",
+    "url": "/react-useTabs/static/css/main.c0eea316.chunk.css"
+  },
+  {
+    "revision": "39616c3cb40ac9211ba2",
+    "url": "/react-useTabs/static/js/2.dc7e6e88.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-useTabs/static/js/2.dc7e6e88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecec7fc8af9194f0a0f2",
+    "url": "/react-useTabs/static/js/main.ba0276b5.chunk.js"
+  },
+  {
+    "revision": "04095be22156aa3d69fc",
+    "url": "/react-useTabs/static/js/runtime-main.af71a743.js"
+  }
+]);
